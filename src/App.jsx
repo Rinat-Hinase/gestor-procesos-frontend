cca30a9a1d39e13c51e126eb4feef0a4206759eb
Rinat-Hinase <maresjefthekiller@gmail.com>
2025-06-03 12:50:14 -0600

    Inicio de proyecto frontend - estructura base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center text-xl font-bold">
+      Gestor de Procesos - Inicio
+    </div>
+  );
+}
+
+export default App;
